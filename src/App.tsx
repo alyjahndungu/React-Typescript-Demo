6900947d88f6b29099fad9dc66ae56ca49a60399
elijah-systems <elijahndungu30@gmail.com>
2020-10-06 12:13:21 +0300

    Errors Handled

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+//import Login from './components/Login';
+  import Register from './components/Register';
+ import SideNav from './components/SideNav';
+ import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <div className="row">
+        <div className="col-sm-4">
+   <SideNav/>
+        </div>
+         <div className="col-sm-4">
+   
+    
+           
+           </div>
+           <Router>
+            <div className="col-sm-4">
+           
+            <Route exact path="/components" component={Register} />
+
+            </div>
+             </Router>
+    </div>
+       
     </div>
   );
 }
---
+++ src/components/Register.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+// import Login from './Login';
+
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        TernstoneHub
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+interface UserRegProps{
+
+}
+
+interface UserRegState{
+  isLoading: boolean;
+}
+
+export default class Register extends React.Component<UserRegProps, UserRegState> {
+     constructor(props: UserRegProps) {
+         
+    super(props);
+    this.state = {isLoading:false};
+
+  }
+  
+render(){
+    
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="form rotateIn">
+      <i className="fas fa-lock  fa-3x  text-secondary"></i>
+
+
+        <Typography component="h1" variant="h5">
+          CREATE ACCOUNT
+        </Typography>
+        <form className="" noValidate>
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+                  <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="fname"
+            label="Firstname"
+            name="fname"
+            autoComplete="fistname"
+            autoFocus
+          />
+
+                  <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="lastname"
+            label="Lastname"
+            name="lname"
+            autoComplete="lastname"
+            autoFocus
+          />
+
+                  <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="phone"
+            label="PhoneNumber"
+            name="phone"
+            autoComplete="phone"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className="btn btn-sm rounded-pill shadow"
+              >
+            Sign Up
+          </Button>
+          
+          <Grid container>
+            <Grid item>
+              <a href="/Login">
+                Have an account? Sign In
+              </a>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+}
