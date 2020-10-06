f8d43e76e5a9034536ca12999814e61051f89b1e
elijah-systems <elijahndungu30@gmail.com>
2020-10-06 12:18:07 +0300

    Errors Handled

--- src/components/material-dashboard-react.tsx
+++ src/components/material-dashboard-react.tsx
@@ -1,11 +1,5 @@
 
 
- * Product Page: http://www.creative-tim.com/product/material-dashboard-react
- * Copyright 2020 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
- =========================================================
-
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
  */
